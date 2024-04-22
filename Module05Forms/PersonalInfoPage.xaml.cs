c15a38894d3288c4b2935ad6698e59888d9ec496
ADMIN <ADMIN@DESKTOP-E8M30O1>
2024-04-22 22:08:17 +0800

    Add project files.

---
+++ Module05Forms/PersonalInfoPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace Module05Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonalInfoPage : ContentPage
+    {
+        public PersonalInfoPage()
+        {
+            InitializeComponent();
+
+            middleNameEntry.IsEnabled = middleNameSwitch.IsToggled;
+            middleNameSwitch.Toggled += (sender, e) =>
+            {
+                middleNameEntry.IsEnabled = e.Value;
+            };
+
+            passwordEntry.IsPassword = true;
+            repeatPasswordEntry.IsPassword = true;
+        }
+
+        private void BirthdatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DateTime birthDate = birthdatePicker.Date;
+            DateTime currentDate = DateTime.Today;
+            int age = currentDate.Year - birthDate.Year;
+            if (currentDate.Month < birthDate.Month || (currentDate.Month == birthDate.Month && currentDate.Day < birthDate.Day))
+            {
+                age--;
+            }
+            ageLabel.Text = age.ToString();
+        }
+
+        private void SubmitButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
